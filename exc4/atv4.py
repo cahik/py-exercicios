07f800dac872ce8dd8b060511458179ae2c885a0
HIK <54104687+cahik@users.noreply.github.com>
2019-12-06 08:26:47 -0300

    ex do aviao. lula ladrao robou meu coracao. :D

---
+++ exc4/atv4.py
@@ -0,0 +1,23 @@
+from classe import *
+
+
+print(terminal)
+print(aviao)
+
+
+indo('piloto','oficial1')
+vindo('piloto')
+indo('piloto', 'oficial2')
+vindo('piloto')
+indo('piloto', 'chefe')
+vindo('chefe')
+indo('chefe', 'comissaria1')
+vindo('chefe')
+indo('chefe','comissaria2')
+vindo('chefe')
+indo('puliça', 'lula')
+vindo('piloto')
+indo('piloto', 'chefe')
+
+print(terminal)
+print(aviao)
---
+++ exc4/classe.py
@@ -0,0 +1,21 @@
+
+
+# Atençao! neste código o prisioneiro = lula e policial = puliça
+
+aviao = []
+terminal = ['piloto','oficial1','oficial2','chefe','comissaria1','comissaria2','lula','puliça']
+
+
+
+def indo( p1,p2):
+    terminal.remove(p1)
+    terminal.remove(p2)
+    aviao.append(p1)
+    aviao.append(p2)
+    print(f'Fortwo em movimento, levando o motorista {p1} e o passageiro {p2} para o avião{aviao}')
+
+def vindo(p1, p2=None):
+    aviao.remove(p1)
+    terminal.append(p1)
+    print(f'Fortwo em movimento, levando o motorista {p1} para o terminal{terminal}')
+
